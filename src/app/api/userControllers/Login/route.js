75221814380cd11e9c32af9b2ca905832232de8b
Naveen Prajapati <prajapatinaveen7003@gmail.com>
2024-10-27 19:50:59 +0530

    login done

--- src/app/api/userControllers/Login/route.js
+++ src/app/api/userControllers/Login/route.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { NextResponse } from "next/server";
+import connectDb from "../../dbConnect/route";
+import { userModel } from "../../Models/userModel/route";
 
-const route = () => {
-  return (
-    <div>route</div>
-  )
+export async function POST(req){
+    await connectDb()
+    try{
+    const body = await req.json();
+    const user = await userModel.findOne({email: body.email}).exec();
+    if(user){
+        return NextResponse.json({message:"user found"})
+    }
+    }catch(error){
+        console.log("User not found")
+    }
 }
-
-export default route
